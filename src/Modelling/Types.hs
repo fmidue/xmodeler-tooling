18ec71290b325d62a5e444396d54f5ac762c5ab3
Ashraf Hashash <33121093+smmohash@users.noreply.github.com>
2022-06-27 20:59:51 +0200

    added function getValue
    This should extract the needed values and prepare them to be inserted as they are.

--- src/Modelling/Types.hs
+++ src/Modelling/Types.hs
@@ -1,5 +1,19 @@
 {-# LANGUAGE QuasiQuotes #-}
 
+getValue :: Type -> String
+getValue t = case t of
+  Auxiliary_MonetaryValue (Just (unit, value)) -> [i|Auxiliary::MonetaryValue(#{value}, Auxiliary::Currency(&quot;#{unit}&quot;, &quot;#{value}&quot;, 1.0))|]
+  Auxiliary_Date (Just (year, month, day)) -> [i|Auxiliary::Date::createDate(#{year}, #{month}, #{day})|]
+  Auxiliary_Currency (Just x) -> [i|Auxiliary::Currency(&quot;$&quot;, &quot;#{map toLower (show x)}&quot;, #{relativeToEur x})|]
+  Auxiliary_Complex (Just _) -> "null"
+  Auxiliary_AuxiliaryClass (Just _) -> "null"
+  XCore_Boolean (Just x) -> map toLower (show x)
+  XCore_Integer (Just x) -> show x
+  XCore_Float (Just x) -> show x
+  XCore_String (Just x) -> [i|#{map ord x}.asString()|]
+  XCore_Element (Just _) -> "null"
+  _ -> error "Invalid Type and/or Value!"
+
 data MetaClass = MetaClass {
   mAbstract :: Bool,
   mLevel :: Int,
