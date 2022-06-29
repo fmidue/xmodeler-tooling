24c0f95f58da6b9ea99701fa973844012762bd53
Ashraf Hashash <33121093+smmohash@users.noreply.github.com>
2022-06-29 19:59:16 +0200

    imports

--- src/Modelling/MLM/ToXModeler.hs
+++ src/Modelling/MLM/ToXModeler.hs
@@ -1,10 +1,18 @@
---{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE QuasiQuotes #-}
 --{-# LANGUAGE NoMonomorphismRestriction #-}
 
+-- remember to export these properly when done testing:
 module Modelling.MLM.ToXModeler where
 
 -- remember to import these properly when done testing:
+import Data.String.Interpolate (i)
+import Data.GraphViz (GraphvizCommand)
+import Diagrams.Points (Point (P))
+import Diagrams.TwoD.Types (V2 (V2))
+import Diagrams.TwoD.GraphViz (layoutGraph, mkGraph, getGraph)
+import Data.Map.Strict (toList)
 import Modelling.MLM.Types
+import Data.Bifunctor (first, second, bimap)
 
 class XModelerable a where
   get :: a -> String
