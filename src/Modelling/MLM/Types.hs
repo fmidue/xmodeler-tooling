1ac14c69779e9a89db3220592d40510d448d7f93
Ashraf Hashash <33121093+smmohash@users.noreply.github.com>
2022-06-29 08:36:48 +0200

    added a needed package
    I needed the function nubSort

--- src/Modelling/MLM/Types.hs
+++ src/Modelling/MLM/Types.hs
@@ -4,6 +4,16 @@
 module Modelling.MLM.Types where
 
 import Data.Maybe (isNothing, fromJust)
+import Data.List.Ordered (nubSort)
+
+class Valid a where
+  valid :: a -> Bool
+
+class ValidAssociation a b where
+  validAssociation :: a -> b -> Bool
+
+class ValidLink a b where
+  validLink :: a -> b -> Bool
 
 data MLM = MLM {
   classes :: [Class],
@@ -11,65 +21,118 @@ data MLM = MLM {
   links :: [Link]
 }
 
-class ValidClass a where
-  valid :: a -> Bool
+instance Valid MLM where
+  valid mlm = let
+    mlmClasses = classes mlm
+    mlmClassesNames = map cName mlmClasses
+    mlmAssociations = associations mlm
+    mlmAssociationsNames = map sName mlmAssociations
+    mlmLinks = links mlm
+    in
+      and [
+        all valid mlmClasses,
+        all (validAssociation mlmClasses) mlmAssociations,
+        all (validLink mlmAssociations) mlmLinks,
+        all (\x -> nubSort x == x) [mlmClassesNames, mlmAssociationsNames] -- I could not find a function to check for duplicates
+      ]
 
 {-
 I am aware of
 "In probably more than 90% of occurrences, use of fromJust is a mistake (i.e., is bad coding)."
 The following just for learning purposes and could eventually probably be avoided by pattern-matching or something:
 -}
-instance ValidClass Class where
-  valid c =  all ((== cLevel c) . cLevel) (parents c)
-          && ( isNothing (isOf c) || cLevel (fromJust (isOf c)) == cLevel c + 1)
 
 data Class = Class {
   isAbstract :: Bool,
-  cLevel :: Int,
+  cLevel :: Level,
   cName :: String,
   parents :: [Class],
-  isOf :: Maybe Class,
+  cIsOf :: Maybe Class,
   attributes :: [Attribute],
   operations :: [Operation]
-}
+} deriving Eq
+
+-- here, I had to use && instead of and [...] because hlint kept buggine me about it for some reason. Did it think that I have only two elements in this list? hmmmm
+instance Valid Class where
+  valid c =
+    all ((== cLevel c) . cLevel) (parents c) &&
+    (isNothing (cIsOf c) || cLevel (fromJust (cIsOf c)) == cLevel c + 1) &&
+    all valid (attributes c)
+
 
 data Attribute = Attribute {
-  tLevel :: Int,
+  tLevel :: Level,
   tName :: String,
   tType :: Type,
   multiplicity :: Multiplicity,
   value :: Maybe Value
-  }
+  } deriving Eq
+
+instance Valid Attribute where
+  valid a = valid (multiplicity a)
 
 data Operation = Operation {
   oLevel :: Int,
   oName :: String,
   oType :: Type,
   body :: String
-  }
+  } deriving Eq
 
 data Association = Association {
   sName :: String,
   sSource :: Class,
   sTarget :: Class,
-  lvlSource :: Int,
-  lvlTarget :: Int,
+  lvlSource :: Level,
+  lvlTarget :: Level,
   multTargetToSource :: Multiplicity,
   multSourceToTarget :: Multiplicity,
   sourceVisibleFromTarget :: Bool,
   targetVisibleFromSource :: Bool
-  }
+  } deriving Eq
+
+
+instance ValidAssociation [Class] Association where
+  validAssociation mlmClasses (Association _ source target
+    lvlS lvlT multST multTS _ _) = and [
+    source `elem` mlmClasses,
+    target `elem` mlmClasses,
+    lvlS < cLevel source,
+    lvlT < cLevel target,
+    all valid [lvlS, lvlT],
+    all valid [multST, multTS]
+    -- might add restrictions to naming, later. For example, you cannot start the name with a digit.
+    ]
 
 data Link = Link {
-  lName :: String,
+  lIsOf :: Association,
   lSource :: Class,
   lTarget :: Class
-  }
+  } deriving Eq
+
+
+instance ValidLink [Association] Link where
+  validLink mlmAssociations (Link isOf source target) = and [
+    isOf `elem` mlmAssociations,
+    source == sSource isOf,
+    target == sTarget isOf,
+    cLevel source == lvlSource isOf,
+    cLevel target == lvlTarget isOf
+    ]
 
 data Multiplicity = Multiplicity {
   lower :: Int,
   upper :: Int
-}
+} deriving Eq
+
+instance Valid Multiplicity where
+  valid (Multiplicity lowerBound upperBound) =
+    lowerBound >= 0 &&
+    lowerBound <= upperBound || upperBound == -1
+
+type Level = Int
+
+instance Valid Level where
+  valid level = level >= 0
 
 type Value = String
 type Type = String
