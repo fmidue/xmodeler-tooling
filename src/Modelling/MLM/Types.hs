c5286623a14c1d66bddaec9d3cf3724f2ff5496e
Ashraf Hashash <33121093+smmohash@users.noreply.github.com>
2022-07-01 02:38:18 +0200

    fixed types and removed others
    removed MonetaryValue
    fixed Value
    re-added relativeToEur
    added currencySymbol
    removed old comments

--- src/Modelling/MLM/Types.hs
+++ src/Modelling/MLM/Types.hs
@@ -145,9 +145,7 @@ instance Valid () Level where
 
 data Type = Boolean | Integer | Float | String | Element | MonetaryValue | Date | Currency | Complex | AuxiliaryClass deriving (Show, Eq)
 
-data MonetaryValue = MonetaryValueString String String | MonetaryValueFloat String Float deriving Eq
-
-data Value = B Bool | I Int | F Float | S String | M MonetaryValue | D Int Int Int | C String | X String | A String deriving Eq
+data Value = B Bool | I Int | F Float | S String | E () | M String String | D Int Int Int | C XModelerCurrency | X String | A String deriving Eq
 
 data Object = Object {
   objName :: String,
@@ -159,17 +157,7 @@ instance Valid [String] [Object] where
   valid mlmClassesNames objects =
     fromList (map objName objects) == fromList mlmClassesNames
 
--- I think these are going to be useful later:
-{-
-
-
-import Data.Char (ord, toLower)
-import Data.String.Interpolate (i)
-import Data.List.Split (splitOn)
-import Data.List (intercalate)
-
-data XModelerCurrency = USD | EUR | GBP | AUD | NZD deriving Show
-
+data XModelerCurrency = USD | EUR | GBP | AUD | NZD deriving (Eq, Show)
 
 relativeToEur :: XModelerCurrency -> Float
 relativeToEur USD = 0.9502091
@@ -178,33 +166,7 @@ relativeToEur GBP = 1.165868
 relativeToEur AUD = 0.65582377
 relativeToEur NZD = 0.5976929
 
-data Type =
-  XCore_Boolean (Maybe Bool) |
-  XCore_Integer (Maybe Int) |
-  XCore_Float (Maybe Float) |
-  XCore_String (Maybe String) |
-  XCore_Element (Maybe ()) |
-  Auxiliary_MonetaryValue (Maybe (String, String)) |
-  Auxiliary_Date (Maybe (Int, Int, Int)) |
-  Auxiliary_Currency (Maybe XModelerCurrency) |
-  Auxiliary_Complex (Maybe String) |
-  Auxiliary_AuxiliaryClass (Maybe String)
-  deriving Show
-
-getType :: Type -> String
-getType =  intercalate "::" . splitOn "_" . head . splitOn " " . show
-
-getValue :: Type -> String
-getValue t = case t of
-  Auxiliary_MonetaryValue (Just (unit, value)) -> [i|Auxiliary::MonetaryValue(#{value}, Auxiliary::Currency(&quot;#{unit}&quot;, &quot;#{value}&quot;, 1.0))|]
-  Auxiliary_Date (Just (year, month, day)) -> [i|Auxiliary::Date::createDate(#{year}, #{month}, #{day})|]
-  Auxiliary_Currency (Just x) -> [i|Auxiliary::Currency(&quot;$&quot;, &quot;#{map toLower (show x)}&quot;, #{relativeToEur x})|]
-  Auxiliary_Complex (Just _) -> "null"
-  Auxiliary_AuxiliaryClass (Just _) -> "null"
-  XCore_Boolean (Just x) -> map toLower (show x)
-  XCore_Integer (Just x) -> show x
-  XCore_Float (Just x) -> show x
-  XCore_String (Just x) -> [i|#{map ord x}.asString()|]
-  XCore_Element (Just _) -> "null"
-  _ -> error "Invalid Type and/or Value!"
--}
+currencySymbol :: XModelerCurrency -> String
+currencySymbol EUR = "EUR"
+currencySymbol GBP = "£"
+currencySymbol _ = "$"
