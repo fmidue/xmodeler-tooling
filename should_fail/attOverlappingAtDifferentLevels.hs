576e894254a4b08e334a4a59e6fe7f4a75469a9b
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2022-09-05 22:07:37 +0200

    attribute cross-checking also needs to be performed across levels

---
+++ should_fail/attOverlappingAtDifferentLevels.hs
@@ -0,0 +1,78 @@
+MLM
+    { name = Name "differentLevels"
+    , classes =
+        [ Class
+            { isAbstract = False
+            , level = 4
+            , name = Name "A"
+            , parents = []
+            , classifier = Nothing
+            , attributes =
+                [ Attribute
+                    { level = 3
+                    , name = Name "another"
+                    , dataType = Integer
+                    , multiplicity = Multiplicity
+                        ( 1
+                        , Just 1
+                        )
+                    }
+                , Attribute
+                    { level = 2
+                    , name = Name "attr"
+                    , dataType = Integer
+                    , multiplicity = Multiplicity
+                        ( 1
+                        , Just 1
+                        )
+                    }
+                ]
+            , operations = []
+            , slots = []
+            }
+        , Class
+            { isAbstract = False
+            , level = 3
+            , name = Name "B"
+            , parents = []
+            , classifier = Just
+                ( Name "A" )
+            , attributes =
+                [ Attribute
+                    { level = 1
+                    , name = Name "attr"
+                    , dataType = Integer
+                    , multiplicity = Multiplicity
+                        ( 1
+                        , Just 1
+                        )
+                    }
+                ]
+            , operations = []
+            , slots =
+                [ Slot
+                    { name = Name "another"
+                    , value = VInteger 0
+                    }
+                ]
+            }
+        , Class
+            { isAbstract = False
+            , level = 2
+            , name = Name "C"
+            , parents = []
+            , classifier = Just
+                ( Name "B" )
+            , attributes = []
+            , operations = []
+            , slots =
+                [ Slot
+                    { name = Name "attr"
+                    , value = VInteger 0
+                    }
+                ]
+            }
+        ]
+    , associations = []
+    , links = []
+    }
