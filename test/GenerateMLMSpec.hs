848afc934b0006004419314dae176d5822a43912
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2022-09-07 13:42:30 +0200

    test related to fmidue/ba-ashraf-hashash#17

--- test/GenerateMLMSpec.hs
+++ test/GenerateMLMSpec.hs
@@ -6,11 +6,18 @@ import Test.QuickCheck (forAll)
 import Config (reasonableConfigs)
 
 import Modelling.MLM.GenerateMLM (generateMLM)
-import Modelling.MLM.Types (valid)
+import Modelling.MLM.Types (valid, MLM(..))
+import Modelling.MLM.Config (Config(..))
 
 spec :: Spec
-spec = describe "generateMLM" $
+spec = do
+        describe "generateMLM" $
           it "creates valid MLMs" $
             forAll reasonableConfigs $ \config ->
             forAll (generateMLM config) $
             valid ()
+        describe "generateMLM" $
+          it "creates as many classes as asked for" $
+            forAll reasonableConfigs $ \config@Config{numClasses0} ->
+            forAll (generateMLM config) $ \MLM{classes} ->
+            length classes == numClasses0
