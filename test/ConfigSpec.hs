addc736330208b73701aee9543011d5565f79090
Ashraf Hashash <33121093+smmohash@users.noreply.github.com>
2022-09-29 04:10:48 +0200

    added test for the new `checkConfig`

--- test/ConfigSpec.hs
+++ test/ConfigSpec.hs
@@ -2,10 +2,10 @@ module ConfigSpec (spec) where
 
 import Test.Hspec (Spec, describe, it)
 import Test.QuickCheck (forAll)
-import Data.Maybe (isNothing)
+import Data.Maybe (isNothing, isJust)
 
 import Config (reasonableConfigs, smallConfigs)
-import Modelling.MLM.Config (checkConfig, defaultConfig)
+import Modelling.MLM.Config (checkConfig, defaultConfig, Config(..))
 
 spec :: Spec
 spec = do
@@ -16,3 +16,20 @@ spec = do
       forAll reasonableConfigs (isNothing . checkConfig)
     it "should accept valid bounds" $
       forAll smallConfigs (isNothing . checkConfig)
+    it "should reject configs with numberOfClasses smaller than 0,  numberOfAssociations smaller than 0, maxClassLevel smaller than 1, numberOfAttributesPerConcretization smaller than 1, tendencies/chances/portions that are not in the range (0,1), multiplicitySpecAssociations second component smaller than 1, or an invalid MLM name" $ all (\x -> isJust (checkConfig x) || error (show x)) [
+        defaultConfig{projectNameString = "abc?"},
+        defaultConfig{projectNameString = "_abc"} ,
+        defaultConfig{projectNameString = "123abc"},
+        defaultConfig{maxClassLevel = 0},
+        defaultConfig{numberOfClasses = -1},
+        defaultConfig{numberOfAssociations = -2},
+        defaultConfig{tendencyToConcretize = 2},
+        defaultConfig{tendencyToInherit = -5},
+        defaultConfig{multiplicitySpecAssociations = (5,5)},
+        defaultConfig{multiplicitySpecAssociations = (0.4,-1)},
+        defaultConfig{chanceVisibleAssociation = 2},
+        defaultConfig{tendencyAbstractClass = -1},
+        defaultConfig{portionOfPossibleLinksToKeep = 200},
+        defaultConfig{numberOfAttributesPerConcretization = 0},
+        defaultConfig{tendencyToDistanceAttributeFromItsInstantiation = -0.1}
+      ]
