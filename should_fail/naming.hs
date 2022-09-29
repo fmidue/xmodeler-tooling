c8926038f7cd92458592f94625aa5f55c3d13f44
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2022-09-29 14:15:14 +0200

    example with referring to "non-existing" name

---
+++ should_fail/naming.hs
@@ -0,0 +1,18 @@
+MLM
+    { name = Name "naming"
+    , classes =
+        [ Class
+            { isAbstract = False
+            , level = 1
+            , name = Name "A"
+            , parents =
+                [ Name "B" ]
+            , classifier = Nothing
+            , attributes = []
+            , operations = []
+            , slots = []
+            }
+        ]
+    , associations = []
+    , links = []
+    }
